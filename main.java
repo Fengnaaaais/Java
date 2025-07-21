9f1ee56c6f6d3651092b9bd289ccc5c5df586f4a
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-21 22:29:20 +0500

    Programs | Procent

--- main.java
+++ main.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
- 
+
 public class Program {
- 
-    public static void main(String[] args) {
- 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int num = in.nextInt();
- 
-        if(num > 2 && num < 9){
-            System.out.println("Число больше 2 и меньше 9");
-        }
-        else{
-            System.out.println("Неизвестное число");
-        }
-        in.close();
+
+  public static void main (String args[]) {
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Сумма вклада: ");
+    int num = in.nextInt();
+    double procent = 1;
+    if (num < 100) {
+      procent += 0.5;
     }
+    else if (num >= 100 && num <= 200) {
+      procent += 0.7;
+    }
+    else {
+      procent += 0.10;
+    }
+    num += procent;
+    System.out.printf("%d", num);
+
+    in.close();
+  }
 }
+
