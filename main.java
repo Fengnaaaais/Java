d6af8b91e641c1a8948f01774a2d23d0cf02678e
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-21 20:20:24 +0500

    Base | Variables

---
+++ main.java
@@ -0,0 +1,21 @@
+public class Program{
+  public static void main (String args[]){
+    float pi = 3.14f;
+    boolean isActive = false;
+    boolean isNotActive = true;
+    final int WIDTH = 768;
+    double num = 21.2;
+    char c = 'c';
+    int x = 21;
+    byte small_num = 3;
+    System.out.println(pi);
+    System.out.println(isActive);
+    System.out.println(isNotActive);
+    System.out.println(num);
+    System.out.println(c);
+    System.out.println(x);
+    System.out.println(small_num);
+    System.out.println(WIDTH);
+  }
+}
+
