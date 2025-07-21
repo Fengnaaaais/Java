feebd6d7142546b3f379f412ad135259e34249f2
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-21 21:01:45 +0500

    Base | Inputs

--- main.java
+++ main.java
@@ -1,7 +1,20 @@
-public class Program{
-  public static void main (String args[]){
-    int x = 10;
-    System.out.printf("number: %d \n", x);
+import java.util.Scanner;
+
+public class Program {
+
+  public static void main (String args[]) {
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter your name: ");
+    String name = in.nextLine();
+
+    System.out.print("Enter your age: ");
+    int num = in.nextInt();
+
+    System.out.printf("Name: %s\tAge: %d\n", name, num);
+
+    in.close();
   }
 }
 
