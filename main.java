64f10396104d424ff5913744a57cec660b1bfb9d
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-22 09:43:54 +0500

    Base | Arrays

--- main.java
+++ main.java
@@ -1,19 +1,15 @@
-import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        boolean isActive = true;
-        while (isActive == true) {
-            System.out.print("Введите первое число: ");
-            int num1 = in.nextInt();
-            System.out.print("Введите второе число: ");
-            int num2 = in.nextInt();
-            System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
-            System.out.print("Хотите продолжить(1 - да): ");
-            if (in.nextInt() != 1) {
-                isActive = false;
-            }
-        }
-        in.close();
+        int nums[];
+        int[] nums_;
+
+        int[] nums1 = new int[] { 1, 2, 3, 4, 5 };
+        System.out.println(nums1[0]);
+        // Equal
+        int[] nums2 = { 1, 2, 3, 4, 5 };
+        System.out.println(nums2[0]);
+        
+        nums1[0] = 2;
+        System.out.println(nums1[0]);
     }
 }
