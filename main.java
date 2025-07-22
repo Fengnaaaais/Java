57d9a61797b8859e2d4579b195b08eb6a2fde42c
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-22 08:32:34 +0500

    Programs | Bank

--- main.java
+++ main.java
@@ -2,41 +2,18 @@ import java.util.Scanner;
 
 public class Program {
 
-  public static void main (String args[]) {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-    Scanner in = new Scanner(System.in);
+        System.out.print("Введите сумму вклада: ");
+        float sum = in.nextFloat();
 
-    System.out.print("Введите первое число: ");
-    int num1 = in.nextInt();
+        System.out.print("Введите срок вклада в месяцах: ");
+        int month = in.nextInt();
 
-
-    System.out.print("Введите второе число: ");
-    int num2 = in.nextInt();
-
-    System.out.print("Введите номер операции: 1.Сложение  2.Вычитание  3.Умножение: ");
-    int opeartion = in.nextInt();
-
-    int res = 0;
-
-    switch (opeartion) 
-    {
-        case 1:
-            res = num1 + num2;
-            break;
-        case 2:
-            res = num1 - num2;
-            break;
-        case 3:
-            res = num1 * num2;
-            break;
-        default:
-            System.out.println("Операция не определена\n");
-            break;
+        for (int i = 0; i < month; i++) {
+            sum *= 1.07;
+        }
+        System.out.printf("После %d месяцев сумма вклада составит %f", month, sum);
     }
-    
-    System.out.printf("Результат операций: %d\n", res);
-
-    in.close();
-  }
 }
-
