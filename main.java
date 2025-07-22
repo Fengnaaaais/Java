afb65ffcb5618d808a427adc6e781b3ef11eeb1d
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-22 08:46:13 +0500

    Programs | Multiplication table

--- main.java
+++ main.java
@@ -1,21 +1,10 @@
-import java.util.Scanner;
-
 public class Program {
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Введите сумму вклада: ");
-        float sum = in.nextFloat();
-
-        System.out.print("Введите срок вклада в месяцах: ");
-        int month = in.nextInt();
-        int i = 1;
-
-        while (i <= month) {
-            sum *= 1.07;
-            i++;
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 9; j++) {
+                System.out.printf("%d\t", i * j);
+            }
+            System.out.println();
         }
-        System.out.printf("После %d месяцев сумма вклада составит %f", month, sum);
     }
 }
