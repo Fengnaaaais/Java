fa93c163f4cf716c43d346cb213b384585ec0d1f
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-23 21:32:16 +0500

    Programs | Compund interest calculator

--- Main.java
+++ Main.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
-import mymath.MathUtils;
-
 public class Main {
     public static void main(String[] args) {
-        String name = "asdf";
-        char firstLetter = 'a';
-        int age = 30;
-        double height = 1.73;
-        boolean isWorking = true;
 
-        System.out.printf("Hello %s\n", name);
-        System.out.printf("Your name starts with a \"%c\"\n", firstLetter);
+        Scanner in = new Scanner(System.in);
 
-        System.out.printf("You are %d years old\n", age);
-        System.out.printf("You are %04d years old\n", age); 
-        System.out.printf("You are %4d years old\n", age); // right justefied padding
-        System.out.printf("You are %-4d years old\n", age); // left justefied padding
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
 
-        System.out.printf("You height is %f m\n", height);
-        System.out.printf("You height is %.2f m\n", height);
-        Sysetm.out.printf("You height is %+.2f m\n", height); // output a plus
-        Sysetm.out.printf("You height is %,.2f m\n", height); // comma grouping separatorz
-        Sysetm.out.printf("You height is %(.2f m\n", height); // if negative enclose in ()
-        Sysetm.out.printf("You height is % .2f m\n", height); // if negative - add "-" else before else space
-        
-        System.out.printf("Status:\tYou are working %b\n", isWorking);
+        System.out.print("Enter the principal amount: ");
+        principal = in.nextDouble();
 
-        System.out.printf("%s is %d years old", name, age);
+        System.out.print("Enter the interest rate (in %): ");
+        rate = in.nextDouble() / 100;
+
+        System.out.print("Enter the # of times compounded per year: ");
+        timesCompounded = in.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = in.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+
+        System.out.printf("The amount after %d years is: $%.2f \n", years, amount);
+
+        in.close();
     }
 }
