3a550bed03c13d0e07e90b20535c01572e8aae54
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-23 21:17:30 +0500

    Base | Printf

--- Main.java
+++ Main.java
@@ -4,26 +4,29 @@ import mymath.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        String name = "asdf";
+        char firstLetter = 'a';
+        int age = 30;
+        double height = 1.73;
+        boolean isWorking = true;
 
-        double radius;
-        double circumference;
-        double area;
-        double volume;
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("Your name starts with a \"%c\"\n", firstLetter);
 
-        System.out.print("Enter the radius: ");
-        radius = in.nextDouble();
+        System.out.printf("You are %d years old\n", age);
+        System.out.printf("You are %04d years old\n", age); 
+        System.out.printf("You are %4d years old\n", age); // right justefied padding
+        System.out.printf("You are %-4d years old\n", age); // left justefied padding
 
-        MathUtils.Circle c = new MathUtils.Circle(radius);
+        System.out.printf("You height is %f m\n", height);
+        System.out.printf("You height is %.2f m\n", height);
+        Sysetm.out.printf("You height is %+.2f m\n", height); // output a plus
+        Sysetm.out.printf("You height is %,.2f m\n", height); // comma grouping separatorz
+        Sysetm.out.printf("You height is %(.2f m\n", height); // if negative enclose in ()
+        Sysetm.out.printf("You height is % .2f m\n", height); // if negative - add "-" else before else space
+        
+        System.out.printf("Status:\tYou are working %b\n", isWorking);
 
-        circumference = c.getCircumference();
-        area = c.getArea();
-        volume = c.getVolume();
-
-        System.out.printf("The circumference is: %.2f cm\n", circumference);
-        System.out.printf("The area is: %.2f cm³\n", area);
-
-
-        in.close();
+        System.out.printf("%s is %d years old", name, age);
     }
 }
