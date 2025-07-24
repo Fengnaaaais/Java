31853c8316afae11dc05058f7c67f007c19a03dd
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-24 08:51:14 +0500

    Programs | Parse email with substring method

--- Main.java
+++ Main.java
@@ -1,55 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    String Name = "     Pshon non   ";
+    Scanner in = new Scanner(System.in);
 
-    // Methods that return's a string
-    Name = Name.trim(); // Delete spaces before, after
-    Name = Name.replace("n", "a"); // Replace all "n" to "a"
-    String NAME = Name.toUpperCase();
-    String name = Name.toLowerCase();
+    String email;
+    String username;
+    String domain;
 
-    // Get info
-    int length = Name.length(); // String's length
-    char letter = Name.charAt(0); // Get char from index
-    int index = Name.indexOf("P"); // Get first index form char
-    int lastIndex = Name.lastIndexOf("o"); // Get last index from char
+    System.out.print("Enter your email: ");
+    email = in.nextLine();
 
-    System.out.println(letter); // P
-    System.out.println(length); // 9
-    System.out.println(index); // 0
-    System.out.println(lastIndex); // 7
-    System.out.println(NAME); // PSHON NON
-    System.out.println(name); // pshon non
+    if (email.contains("@")) {
+      username = email.substring(0, email.indexOf("@"));
+      domain = email.substring(email.indexOf("@") + 1);
 
-    /*
-    Is empty
-    if (Name.isEmpty()) {
-      System.out.println("Your name is empty"); // 
+      System.out.println(email);
+      System.out.printf("Your username: %s\n", username);
+      System.out.printf("Your domain: %s\n", domain);
     } else {
-      System.out.printf("Hello %s", Name);
+      System.out.println("Email must contain \"@\"");
     }
 
-    Is name contains space
-    if (Name.contains(" ")) {
-      System.out.println("Your name contains a space");
-    } else {
-      System.out.println("Your name doesn't contain any space ")
-    }
 
-    Is equal without ignore case
-    if (Name.equals("Fengnaaaais")) {
-      System.out.prinln("This name is busy");
-    } else {
-      System.out.println("You can use this name!");
-    }
-
-    Is equal with ignore case
-    if (Name.equalsIgnoreCase("Fengnaaaais")) {
-      System.out.prinln("This name is busy");
-    } else {
-      System.out.println("You can use this name!");
-    }
-    */
-
+    in.close();
   }
 }
