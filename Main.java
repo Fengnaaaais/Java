da45d58b3844dc7fe84681eb21db5c8b5860ec81
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-24 08:21:59 +0500

    Base | String Methods

--- Main.java
+++ Main.java
@@ -1,32 +1,55 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    String Name = "     Pshon non   ";
 
-        Scanner in = new Scanner(System.in);
+    // Methods that return's a string
+    Name = Name.trim(); // Delete spaces before, after
+    Name = Name.replace("n", "a"); // Replace all "n" to "a"
+    String NAME = Name.toUpperCase();
+    String name = Name.toLowerCase();
 
-        double principal;
-        double rate;
-        int timesCompounded;
-        int years;
-        double amount;
+    // Get info
+    int length = Name.length(); // String's length
+    char letter = Name.charAt(0); // Get char from index
+    int index = Name.indexOf("P"); // Get first index form char
+    int lastIndex = Name.lastIndexOf("o"); // Get last index from char
 
-        System.out.print("Enter the principal amount: ");
-        principal = in.nextDouble();
+    System.out.println(letter); // P
+    System.out.println(length); // 9
+    System.out.println(index); // 0
+    System.out.println(lastIndex); // 7
+    System.out.println(NAME); // PSHON NON
+    System.out.println(name); // pshon non
 
-        System.out.print("Enter the interest rate (in %): ");
-        rate = in.nextDouble() / 100;
-
-        System.out.print("Enter the # of times compounded per year: ");
-        timesCompounded = in.nextInt();
-
-        System.out.print("Enter the # of years: ");
-        years = in.nextInt();
-
-        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
-
-        System.out.printf("The amount after %d years is: $%.2f \n", years, amount);
-
-        in.close();
+    /*
+    Is empty
+    if (Name.isEmpty()) {
+      System.out.println("Your name is empty"); // 
+    } else {
+      System.out.printf("Hello %s", Name);
     }
+
+    Is name contains space
+    if (Name.contains(" ")) {
+      System.out.println("Your name contains a space");
+    } else {
+      System.out.println("Your name doesn't contain any space ")
+    }
+
+    Is equal without ignore case
+    if (Name.equals("Fengnaaaais")) {
+      System.out.prinln("This name is busy");
+    } else {
+      System.out.println("You can use this name!");
+    }
+
+    Is equal with ignore case
+    if (Name.equalsIgnoreCase("Fengnaaaais")) {
+      System.out.prinln("This name is busy");
+    } else {
+      System.out.println("You can use this name!");
+    }
+    */
+
+  }
 }
