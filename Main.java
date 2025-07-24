b89eb0c4a2b82c3cd3ae243d1064393eb16e3fb5
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-24 12:19:04 +0500

    Programs | Calculator

--- Main.java
+++ Main.java
@@ -4,17 +4,42 @@ public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
-    String day;
+    double num1;
+    double num2;
+    char operator;
+    double result = 0;
+    boolean validOperation = true;
 
-    System.out.print("Enter the day of the week: ");
-    day = in.nextLine();
+    System.out.print("Enter the first number(Use a comma for decimal numbers (Russian locale).): ");
+    num1 = in.nextDouble();
 
-    switch (day) {
-      case "Monday", "Tuesday", "Wednesday", "Thursday", "Firday" ->
-          System.out.println("It is weekday :)")
-      case "Saturday", "Sunday" -> 
-          System.out.prinln("It is weekend (:")
-      default -> System.out.printf("%s is not a day of week", day);
+    System.out.print("Enter the operator(+, -, *, /, ^): ");
+    operator = in.next().charAt(0);
+
+    System.out.print("Enter the second number(Use a comma for decimal numbers (Russian locale)): ");
+    num2 = in.nextDouble();
+
+    switch (operator) {
+      case '+' -> result = num1 + num2;
+      case '-' -> result = num1 - num2;
+      case '*' -> result = num1 * num2;
+      case '/' -> {
+        if (num2 == 0) {
+          System.out.println("Cannot devide by zero!");
+          validOperation = false;
+        } else {
+          result = num1 / num2;
+        }
+      }
+      case '^' -> result = Math.pow(num1, num2);
+      default -> {
+        System.out.println("Operator " + operator + " does not excist");
+        validOperation = false;
+      }
+    }
+
+    if (validOperation) {
+      System.out.println(result);
     }
 
     in.close();
