23138a71d79efd7b44d24765b8f365486cdad12f
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-25 10:55:03 +0500

    Programs | Number guesting game

--- Main.java
+++ Main.java
@@ -1,47 +1,40 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Random random = new Random();
     Scanner in = new Scanner(System.in);
 
-    double num1;
-    double num2;
-    char operator;
-    double result = 0;
-    boolean validOperation = true;
+    int guess;
+    int attempts = 0;
+    int min = 1;
+    int max = 100;
+    int randomNumber = random.nextInt(min, max + 1);
 
-    System.out.print("Enter the first number(Use a comma for decimal numbers (Russian locale).): ");
-    num1 = in.nextDouble();
+    System.out.println("Number Guesting Game!");
+    System.out.printf("Guess a number between %d-%d\n", min, max);
 
-    System.out.print("Enter the operator(+, -, *, /, ^): ");
-    operator = in.next().charAt(0);
-
-    System.out.print("Enter the second number(Use a comma for decimal numbers (Russian locale)): ");
-    num2 = in.nextDouble();
-
-    switch (operator) {
-      case '+' -> result = num1 + num2;
-      case '-' -> result = num1 - num2;
-      case '*' -> result = num1 * num2;
-      case '/' -> {
-        if (num2 == 0) {
-          System.out.println("Cannot devide by zero!");
-          validOperation = false;
-        } else {
-          result = num1 / num2;
-        }
+    do {
+      System.out.print("Enter the guess: ");
+      guess = in.nextInt();
+      attempts++;
+      
+      if (guess < randomNumber) 
+      {
+        System.out.println("TOO LOW! Try again");
       }
-      case '^' -> result = Math.pow(num1, num2);
-      default -> {
-        System.out.println("Operator " + operator + " does not excist");
-        validOperation = false;
+      else if (guess > randomNumber)
+      {
+        System.out.println("TOO HIGH! Try again");
       }
-    }
+      else 
+      {
+        System.out.printf("You have won! The number was %d\n", randomNumber);
+        System.out.printf("Atempts - %d", attempts);
+      }
 
-    if (validOperation) {
-      System.out.println(result);
-    }
+    } while (guess != randomNumber);
 
-    in.close();
   }
 }
