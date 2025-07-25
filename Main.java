fb1ca3440cd3a8bf49fb23c6c02af0599fec11a4
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-25 19:36:12 +0500

    Base | Arrays 1

--- Main.java
+++ Main.java
@@ -1,81 +1,23 @@
-import java.util.Scanner;
-import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Random random = new Random();
 
-        System.out.print("Enter the # of dice to roll: ");
-        int numOfDice = in.nextInt();
-        int total = 0;
+      String[] fruits = {
+        "apple",
+        "orange",
+        "banana",
+        "watermelon"
+      };
 
-        if (numOfDice > 0) {
-            for (int i = 0; i < numOfDice; i++) {
-                int roll = random.nextInt(6) + 1;  // [0..5] + 1 = [1..6]
-                printDie(roll);
-                System.out.printf("You rolled: %d\n", roll);
-                total += roll;
-            }
-            System.out.printf("Total: %d\n", total);
-        } else {
-            System.out.println("# of dice must be greater than 0");
-        }
+      // int numOfFruits = fruits.length; return how many elemnts in array
+      // Arrays.sort(fruits); sort by alphabit
+      // Arrays.fill(fruits, "pineapple"); // set pineapple to all of elements in array
+      for (String fruit : fruits) {
+        System.out.println(fruit);
+      }
 
-        in.close();
     }
 
-    static void printDie(int roll) {
-        String[] dices = {
-            """
-                 -------
-                |       |
-                |   ●   |
-                |       |
-                 -------
-            """,
-            """
-                 -------
-                | ●     |
-                |       |
-                |     ● |
-                 -------
-            """,
-            """
-                 -------
-                | ●     |
-                |   ●   |
-                |     ● |
-                 -------
-            """,
-            """
-                 -------
-                | ●   ● |
-                |       |
-                | ●   ● |
-                 -------
-            """,
-            """
-                 -------
-                | ●   ● |
-                |   ●   |
-                | ●   ● |
-                 -------
-            """,
-            """
-                 -------
-                | ●   ● |
-                | ●   ● |
-                | ●   ● |
-                 -------
-            """
-        };
-
-        if (roll >= 1 && roll <= 6) {
-            System.out.println(dices[roll - 1]);
-        } else {
-            System.out.println("Invalid die value: " + roll);
-        }
-    }
 }
 
