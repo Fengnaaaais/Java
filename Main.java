ca13a85afa5f91e21a56bc1223a9b27bbee69d8d
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-26 09:54:06 +0500

    Base | Arrays - search element in int array

--- Main.java
+++ Main.java
@@ -1,28 +1,26 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
       Scanner in = new Scanner(System.in);
 
-      String[] foods;
-      int size;
+      int[] numbers = {1, 2, 4, 5, 2, 5, 0};
 
-      System.out.print("What # of food do you want?: ");
-      size = in.nextInt();
-      in.nextLine();
+      System.out.print("Enter the element: ");
+      int target = in.nextInt();
+      boolean isFound = false;
 
-      foods = new String[size];
-
-      for (int i = 0; i < foods.length; i++) {
-        System.out.print("Enter a food: ");
-        foods[i] = in.nextLine();
+      for (int i = 0; i < numbers.length; i++) {
+        if (target == numbers[i]) {
+          System.out.printf("Element found at index: %d\n", i);
+          isFound = true;
+          break;
+        } 
       }
 
-      for (String food : foods) {
-        System.out.println(food);
+      if (!isFound) {
+        System.out.println("Element not found in the array");
       }
-
       in.close();
     }
 }
