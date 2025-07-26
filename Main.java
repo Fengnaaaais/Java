b8f53a4ef64f199e1ed62259169f5c4d78fee3f4
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-26 10:52:24 +0500

    Base | 2D Arrays - show telephone

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
-        String[][] groceries = {
-            {"apple", "banane", "orange"},
-            {"potato", "onion", "carrot"},
-            {"chicken", "pork", "beef"}
+        char[][] telephone = {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'},
+            {'*', '0', '#'}
         };
 
-        groceries[2][1] = "meat"; // replace pork -> meat [row index][element index]
-        
-        for (String[] foods : groceries) {
-            for (String food : foods) {
-                System.out.print(food + " ");
+        for (char[] row : telephone) {
+
+            for (char character : row) {
+                System.out.print(character + "   ");
             }
+            
             System.out.println();
         }
-
     }
 }
 
