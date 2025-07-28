dd2d5463fff70eddca38346c266f3a6c50f286c8
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-28 21:38:59 +0500

    Getters and Setters

--- Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog extends Animal {
-  @Override
-  void speak() {
-    System.out.println("Dog speaking *woof*");
-  }
-}
--- Main.java
+++ Main.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    Car car = new Car("Toyota", "Black", 20000);
 
-    int choice;
-    Animal animal;
+    car.setColor("Red");
+    car.setPrice(150000);
 
-    System.out.print("Would you like dog or cat (1 - dog 2 - cat): ");
-    choice = in.nextInt();
-
-    animal = (choice == 1) ? new Dog() : new Cat();
-
-    animal.speak();
-
-    in.close();
+    System.out.println(car.getInfo());
   }
 }
