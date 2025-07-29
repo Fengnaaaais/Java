4707481b1037ee681c2998af0127c5090092a6c8
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-29 11:02:38 +0500

    Collections | ArrayList 1

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
   public static void main(String[] args) {
-    // Wrapper classes - Allow primitive values (int, double, char, boolean)
-    // Integer - class ; int - primitive
-    // primitive types doesn't have methods, attributes
-    // Allows use in Collections Framework
-    // Example - HashMap<String, Double> because we can create object Double's object
-    String a;
-    Integer b;
-    Double c;
-    Character d;
-    Boolean f;
+    ArrayList<String> fruits = new ArrayList<>();
+
+    fruits.add("Apple");
+    fruits.add("Orange");
+    fruits.add("Banana");
+
+    // fruits.remove(2); // - remove Banana
+    // fruits.set(1, "Pineaple"); // - set Orange - Pineaple
+    // System.out.println(fruits.get(0)); // get element by index
+    // System.out.println(fruits.size());
+
+    Collections.sort(fruits);
+    System.out.println(fruits);
   }
 }
