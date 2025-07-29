e8e16d0cc1b3d8ddcfbf7fc14fd9debec72c1461
Fengnaaaais <tajibay.baglan@gmail.com>
2025-07-29 10:15:28 +0500

    Base | Wrapper classes

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    Car car = new Car("Corvette", 2025, "V8");
-
-    System.out.print(car.getInfo());
-    car.start();
+    // Wrapper classes - Allow primitive values (int, double, char, boolean)
+    // Integer - class ; int - primitive
+    // primitive types doesn't have methods, attributes
+    // Allows use in Collections Framework
+    // Example - HashMap<String, Double> because we can create object Double's object
+    String a;
+    Integer b;
+    Double c;
+    Character d;
+    Boolean f;
   }
 }
